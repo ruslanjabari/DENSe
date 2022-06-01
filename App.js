255ab6079d59681b806b06e803ce548888a7757b
Jerry Chen <jerrychen109@gmail.com>
2022-06-01 15:16:19 -0700

    skeleton of message receiving/processing

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   SafeAreaView,
   StyleSheet,
@@ -13,43 +13,52 @@ import {
   // PermissionsAndroid,
   FlatList,
   TouchableHighlight,
-} from 'react-native';
+} from "react-native";
 
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
-import BleManager from 'react-native-ble-manager';
+import BleManager from "react-native-ble-manager";
 const BleManagerModule = NativeModules.BleManager;
 const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
 
 // nb: react-native-crypto has a dependency issue;
 // library used seems less good but ok for our purposes
 // https://github.com/juhoen/hybrid-crypto-js
-import { Crypt, RSA } from 'hybrid-crypto-js';
-import 'react-native-get-random-values';
+import { Crypt, RSA } from "hybrid-crypto-js";
+import "react-native-get-random-values";
+
+const KEYSHARE_HEADER = "DENSE keyshare";
+const EXPOSURE_HEADER = "DENSE exposure";
 
 const App = () => {
   const [isScanning, setIsScanning] = React.useState(false);
   const peripherals = new Map();
   const [list, setList] = React.useState([]);
   var publicKey, privateKey;
-  const crypt = new Crypt({ md: 'sha256' });
+  const crypt = new Crypt({ md: "sha256" });
 
   React.useEffect(() => {
     // Initialize public and private keys
     var rsa = new RSA({ entropy: 244 });
-    rsa.generateKeyPair(function(keys) {
-        console.log(keys);
-        publicKey = keys.publicKey;
-        privateKey = keys.privateKey;
+    rsa.generateKeyPair(function (keys) {
+      console.log(keys);
+      publicKey = keys.publicKey;
+      privateKey = keys.privateKey;
     }, 1024);
 
     BleManager.start({ showAlert: false });
 
-    bleManagerEmitter.addListener('BleManagerDiscoverPeripheral', handleDiscoverPeripheral);
-    bleManagerEmitter.addListener('BleManagerStopScan', handleStopScan);
-    bleManagerEmitter.addListener('BleManagerDisconnectPeripheral', handleDisconnectedPeripheral);
     bleManagerEmitter.addListener(
-      'BleManagerDidUpdateValueForCharacteristic',
+      "BleManagerDiscoverPeripheral",
+      handleDiscoverPeripheral
+    );
+    bleManagerEmitter.addListener("BleManagerStopScan", handleStopScan);
+    bleManagerEmitter.addListener(
+      "BleManagerDisconnectPeripheral",
+      handleDisconnectedPeripheral
+    );
+    bleManagerEmitter.addListener(
+      "BleManagerDidUpdateValueForCharacteristic",
       handleUpdateValueForCharacteristic
     );
   }, []);
@@ -59,7 +68,7 @@ const App = () => {
       // scan for 5 seconds
       BleManager.scan([], 5, true)
         .then((results) => {
-          console.log('Scanning...');
+          console.log("Scanning...");
           setIsScanning(true);
         })
         .catch((err) => {
@@ -69,7 +78,7 @@ const App = () => {
   };
 
   const handleStopScan = () => {
-    console.log('Scan is stopped');
+    console.log("Scan is stopped");
     setIsScanning(false);
   };
 
@@ -80,12 +89,15 @@ const App = () => {
       peripherals.set(peripheral.id, peripheral);
       setList(Array.from(peripherals.values()));
     }
-    console.log('Disconnected from ' + data.peripheral);
+    console.log("Disconnected from " + data.peripheral);
   };
 
   const handleUpdateValueForCharacteristic = (data) => {
     console.log(
-      'Received data from ' + data.peripheral + ' characteristic ' + data.characteristic,
+      "Received data from " +
+        data.peripheral +
+        " characteristic " +
+        data.characteristic,
       data.value
     );
   };
@@ -93,7 +105,7 @@ const App = () => {
   const retrieveConnected = () => {
     BleManager.getConnectedPeripherals([]).then((results) => {
       if (results.length == 0) {
-        console.log('No connected peripherals');
+        console.log("No connected peripherals");
       }
       console.log(results);
       for (var i = 0; i < results.length; i++) {
@@ -106,9 +118,9 @@ const App = () => {
   };
 
   const handleDiscoverPeripheral = (peripheral) => {
-    console.log('Got ble peripheral', peripheral);
+    console.log("Got ble peripheral", peripheral);
     if (!peripheral.name) {
-      peripheral.name = 'NO NAME';
+      peripheral.name = "NO NAME";
     }
     peripherals.set(peripheral.id, peripheral);
     setList(Array.from(peripherals.values()));
@@ -127,69 +139,73 @@ const App = () => {
               peripherals.set(peripheral.id, p);
               setList(Array.from(peripherals.values()));
             }
-            console.log('Connected to ' + peripheral.id);
+            console.log("Connected to " + peripheral.id);
 
             setTimeout(() => {
               /* Test read current RSSI value */
-              BleManager.retrieveServices(peripheral.id).then((peripheralData) => {
-                console.log('Retrieved peripheral services', peripheralData);
+              BleManager.retrieveServices(peripheral.id).then(
+                (peripheralData) => {
+                  console.log("Retrieved peripheral services", peripheralData);
 
-                BleManager.readRSSI(peripheral.id).then((rssi) => {
-                  console.log('Retrieved actual RSSI value', rssi);
-                  let p = peripherals.get(peripheral.id);
-                  if (p) {
-                    p.rssi = rssi;
-                    peripherals.set(peripheral.id, p);
-                    setList(Array.from(peripherals.values()));
-                  }
-                });
-              });
+                  BleManager.readRSSI(peripheral.id).then((rssi) => {
+                    console.log("Retrieved actual RSSI value", rssi);
+                    let p = peripherals.get(peripheral.id);
+                    if (p) {
+                      p.rssi = rssi;
+                      peripherals.set(peripheral.id, p);
+                      setList(Array.from(peripherals.values()));
+                    }
+                  });
+                }
+              );
             }, 900);
           })
           .catch((error) => {
-            console.log('Connection error', error);
+            console.log("Connection error", error);
           });
       }
     }
   };
 
   const genKeyPair = () => {
-      rsa = new RSA({ entropy: crypto.getRandomValues(new Int32Array([244, 100])) });
-      rsa.generateKeyPair(function(keys) {
-          console.log(keys);
-          publicKey = keys.publicKey;
-          privateKey = keys.privateKey;
-      }, 1024);
-  }
+    rsa = new RSA({
+      entropy: crypto.getRandomValues(new Int32Array([244, 100])),
+    });
+    rsa.generateKeyPair(function (keys) {
+      console.log(keys);
+      publicKey = keys.publicKey;
+      privateKey = keys.privateKey;
+    }, 1024);
+  };
 
   const testCrypto = () => {
-      var toEncrypt = crypto.getRandomValues(new Int32Array([244, 100]));
-      var encrypted = crypt.encrypt(publicKey, toEncrypt);
-      var decrypted = crypt.decrypt(privateKey, encrypted);
-      console.log("Encrypted", toEncrypt, " and got back", decrypted);
-  }
+    var toEncrypt = crypto.getRandomValues(new Int32Array([244, 100]));
+    var encrypted = crypt.encrypt(publicKey, toEncrypt);
+    var decrypted = crypt.decrypt(privateKey, encrypted);
+    console.log("Encrypted", toEncrypt, " and got back", decrypted);
+  };
 
   /* Generate a trivial key advertisement message.
    */
   const genKeyShareMessage = () => {
-      /* TODO: maybe include and sign time, to prevent someone from advertising
+    /* TODO: maybe include and sign time, to prevent someone from advertising
       someone else's key with a copy of the message? */
-      const msg = {header: "DENSE keyshare", message: publicKey};
-      return msg;
-  }
+    const msg = { header: KEYSHARE_HEADER, message: publicKey };
+    return msg;
+  };
 
   const sanityCheck = (encrypted) => {
-      const decrypted = crypt.decrypt(privateKey, encrypted);
-      const verified = crypt.verify(
-          publicKey,
-          decrypted.signature,
-          decrypted.message,
-      );
-      if (verified) {
-          console.log("Encryption and signature passes sanity check!")
-          console.log("Decrypted message: ", decrypted.message)
-      }
-  }
+    const decrypted = crypt.decrypt(privateKey, encrypted);
+    const verified = crypt.verify(
+      publicKey,
+      decrypted.signature,
+      decrypted.message
+    );
+    if (verified) {
+      console.log("Encryption and signature passes sanity check!");
+      console.log("Decrypted message: ", decrypted.message);
+    }
+  };
 
   /* Generate an exposure notification message. Messages include a header,
    * time (if >a week ago, stop forwarding), and signed, encrypted
@@ -197,39 +213,111 @@ const App = () => {
    * as the message may be forwarded by intermediaries who shouldn't know the sender's identity.)
    */
   const genExposureNotification = () => {
-      const time = Date.now();
-      const salt = crypto.getRandomValues(new Int32Array([244]))[0];
-      const message = publicKey + "\n" + time + "\n" + salt;
-      const signature = crypt.signature(privateKey, message);
+    const time = Date.now();
+    const salt = crypto.getRandomValues(new Int32Array([244]))[0];
+    const message = {
+      sender_pk: publicKey,
+      time: time,
+      salt: salt,
+    };
+    const signature = crypt.signature(privateKey, message);
 
-      /* TODO: update to encrypt with the public keys of receivers from log,
+    /* TODO: update to encrypt with the public keys of receivers from log,
       in place of sender's public key, and remove sanity check.*/
-      const encrypted = crypt.encrypt([publicKey, publicKey], message, signature);
-      const msg = {header: "DENSE exposure", time: time, message: encrypted};
-      sanityCheck(msg.message);
+    const encrypted = crypt.encrypt(
+      [publicKey, publicKey],
+      JSON.stringify(message),
+      signature
+    );
+    const msg = { header: EXPOSURE_HEADER, time: time, message: encrypted };
+    sanityCheck(msg.message);
 
-      return msg;
-  }
+    return msg;
+  };
+
+  /* Processes a key-share message. The message should be of the following format:
+   * - header: should be equal to KEYSHARE_HEADER
+   * - message: should contain a public key
+   */
+  const processKeyShareMessage = (msg) => {
+    const time = Date.now();
+    const pk = msg.message;
+    const nonce = crypto.randomBytes(16); // 128-bit nonce
+
+    console.log(`received keyshare message at time ${time}: (${pk}, ${nonce})`);
+
+    // TODO: log (time, pk, nonce)
+  };
+
+  /* Processes an exposure notification message. The message should be of the following format:
+   * - header: should be equal to EXPOSURE_HEADER
+   * - time: time when message was sent
+   * - message: JSON object { pk, time, salt } encrypted with the recipient's public key
+   */
+  const processExposureMessage = (msg) => {
+    // ignore message if it's older than a week ago
+    if (Date.now() - msg.time >= 7 * 24 * 60 * 60 * 1000) {
+      console.log(`ignoring stale exposure message`);
+      return;
+    }
+
+    // TODO: hash message and check notification log to see if this message has already been received
+
+    const decrypted = crypt.decrypt(privateKey, msg.message);
+    const message = JSON.parse(decrypted.message);
+
+    const verified = crypt.verify(
+      message.sender_pk,
+      decrypted.signature,
+      decrypted.message
+    );
+
+    if (!verified) {
+      console.log("Received exposure message with incorrect signature");
+      return;
+    }
+
+    // TODO: search logs for message.sender_pk time of encounter and alert user
+
+    // TODO: broadcast the same message to nearby devices
+
+    // TODO: add message to notification log
+  };
 
   const renderItem = (item) => {
-    const color = item.connected ? 'green' : '#fff';
+    const color = item.connected ? "green" : "#fff";
     return (
       <TouchableHighlight onPress={() => testPeripheral(item)}>
         <View style={[styles.row, { backgroundColor: color }]}>
-          <Text style={{ fontSize: 12, textAlign: 'center', color: '#333333', padding: 10 }}>
+          <Text
+            style={{
+              fontSize: 12,
+              textAlign: "center",
+              color: "#333333",
+              padding: 10,
+            }}
+          >
             {item.name}
           </Text>
-          <Text style={{ fontSize: 10, textAlign: 'center', color: '#333333', padding: 2 }}>
+          <Text
+            style={{
+              fontSize: 10,
+              textAlign: "center",
+              color: "#333333",
+              padding: 2,
+            }}
+          >
             RSSI: {item.rssi}
           </Text>
           <Text
             style={{
               fontSize: 8,
-              textAlign: 'center',
-              color: '#333333',
+              textAlign: "center",
+              color: "#333333",
               padding: 2,
               paddingBottom: 20,
-            }}>
+            }}
+          >
             {item.id}
           </Text>
         </View>
@@ -241,7 +329,10 @@ const App = () => {
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView}>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}
+        >
           {global.HermesInternal == null ? null : (
             <View style={styles.engine}>
               <Text style={styles.footer}>Engine: Hermes</Text>
@@ -250,35 +341,49 @@ const App = () => {
           <View style={styles.body}>
             <View style={{ margin: 10 }}>
               <Button
-                title={'Scan Bluetooth (' + (isScanning ? 'on' : 'off') + ')'}
+                title={"Scan Bluetooth (" + (isScanning ? "on" : "off") + ")"}
                 onPress={() => startScan()}
               />
             </View>
 
             <View style={{ margin: 10 }}>
-              <Button title="Retrieve connected peripherals" onPress={() => retrieveConnected()} />
+              <Button
+                title="Retrieve connected peripherals"
+                onPress={() => retrieveConnected()}
+              />
             </View>
 
             <View style={{ margin: 10 }}>
-              <Button title="Generate a fresh key pair (may take a while)" onPress={() => genKeyPair()} />
+              <Button
+                title="Generate a fresh key pair (may take a while)"
+                onPress={() => genKeyPair()}
+              />
             </View>
 
             <View style={{ margin: 10 }}>
-              <Button title="Encrypt and decrypt a message" onPress={() => testCrypto()} />
+              <Button
+                title="Encrypt and decrypt a message"
+                onPress={() => testCrypto()}
+              />
             </View>
 
             <View style={{ margin: 10 }}>
-              <Button title="Print a key share to the console" onPress={() => console.log(genKeyShareMessage())} />
+              <Button
+                title="Print a key share to the console"
+                onPress={() => console.log(genKeyShareMessage())}
+              />
             </View>
 
             <View style={{ margin: 10 }}>
-              <Button title="Print an exposure notification to the console"
-                onPress={() => console.log(genExposureNotification())} />
+              <Button
+                title="Print an exposure notification to the console"
+                onPress={() => console.log(genExposureNotification())}
+              />
             </View>
 
             {list.length == 0 && (
               <View style={{ flex: 1, margin: 20 }}>
-                <Text style={{ textAlign: 'center' }}>No peripherals</Text>
+                <Text style={{ textAlign: "center" }}>No peripherals</Text>
               </View>
             )}
           </View>
@@ -298,7 +403,7 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.lighter,
   },
   engine: {
-    position: 'absolute',
+    position: "absolute",
     right: 0,
   },
   body: {
@@ -310,25 +415,25 @@ const styles = StyleSheet.create({
   },
   sectionTitle: {
     fontSize: 24,
-    fontWeight: '600',
+    fontWeight: "600",
     color: Colors.black,
   },
   sectionDescription: {
     marginTop: 8,
     fontSize: 18,
-    fontWeight: '400',
+    fontWeight: "400",
     color: Colors.dark,
   },
   highlight: {
-    fontWeight: '700',
+    fontWeight: "700",
   },
   footer: {
     color: Colors.dark,
     fontSize: 12,
-    fontWeight: '600',
+    fontWeight: "600",
     padding: 4,
     paddingRight: 12,
-    textAlign: 'right',
+    textAlign: "right",
   },
 });
 
